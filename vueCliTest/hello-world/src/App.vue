c8fc2fdca7339f83a8784bc092eec01636da81a6
Ling137 <1375025651@qq.com>
2020-01-31 13:49:56 +0800

    ignore

---
+++ vueCliTest/hello-world/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="http://p9.qhimg.com/bdm/340_210_0/t01feb5393e822c1fca.jpg">
+    <!-- <img alt="Vue logo" src="http://p5.qhimg.com/bdm/340_210_0/t013c1ac7dd36a3e750.jpg"> -->
+    <HelloWorld msg="Welcome to 2020 Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
